f5e6fdd27595352b90d688bee2087dc8d786fda9
regginamarshandi17 <marshandireggina@gmail.com>
2023-12-03 22:03:20 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <center><h1>SELAMAT DATANG</h1></center>
+    <hr>
+    <center><div class="container">
+        <h1>Login</h1>
+        <form>
+            <input
+            type="text"
+            class="inputa"
+            placeholder="Masukan Username"
+            />
+            <input
+            type="password"
+            class="inputa"
+            placeholder="Masukan Password"
+            />
+            <input type="submit" class="wed" />
+            <input type="reset" class="wed" />
+        </form>
+    </div></center>
+</body>
+</html>
